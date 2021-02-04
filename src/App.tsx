dedb1a8757fa8833de7be97d22547d3cfd4ee1ee
Neosoulink <neodarksoulink@gmail.com>
2021-02-04 17:37:58 +0100

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./assets/css/style.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<div className="bg-img"></div>
+			<div className="content">
+				<h1>Hello Quiz</h1>
+			</div>
+		</div>
+	);
 }
 
 export default App;

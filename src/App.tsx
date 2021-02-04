0642a2a885224e0e8aa411673a89406d9936ad13
Neosoulink <neodarksoulink@gmail.com>
2021-02-04 18:06:04 +0100

    Create and set right files

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import QuestionCard from './components/QuestionCard';
 import "./assets/css/style.css";
 
 function App() {
---
+++ src/components/QuestionCard.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function QuestionCard() {
+	return (
+		<div>
+			Question card
+		</div>
+	)
+}
+
+export default QuestionCard
